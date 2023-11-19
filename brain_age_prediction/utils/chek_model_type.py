ef84b182e676a9639c2f41e7ebcc7993c59129cf
Lorenzo Zaffina <zaffinalorenzo@gmail.com>
2023-11-19 16:16:54 +0100

    improved docstring

--- brain_age_prediction/utils/chek_model_type.py
+++ brain_age_prediction/utils/chek_model_type.py
@@ -8,6 +8,9 @@ def check_model_type(model_type):
     '''
     Check if 'model_type' is either 'structural', 'functional' or 'joint'
     If not, raise an AssertionError
+
+        Parameters:
+                    model_type (string): the string indicating the model type
     '''
 
     assert model_type in ('structural','functional','joint'),(
